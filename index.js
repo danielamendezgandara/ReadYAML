9dbd317ec00ad3855b383248b2bf07b7026d0003
danielamendezgandara <daniela.a.mendezgandara@gmail.com>
2021-10-20 23:55:31 -0300

    Create file yaml

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const yaml = require('js-yaml');
 const fs   = require('fs');
+const YAML = require('yaml');
 
 // Get document, or throw exception on error
 try {
@@ -9,3 +10,32 @@ try {
 } catch (e) {
   console.log(e);
 }
+
+var stream = fs.createWriteStream("nuevo2.yml");
+stream.once('open', function(fd) {
+  stream.write("Primera línea\n");
+  stream.write("Segunda línea\n");
+  stream.end();
+});
+
+
+
+const jsonObject = {
+    version: "1.0.0",
+    dependencies: {
+        yaml: "^1.10.0"
+    },
+    package: {
+        exclude: [ ".idea/**", ".gitignore" ]
+    }
+}
+
+const doc = new YAML.Document();
+doc.contents = jsonObject;
+
+console.log(doc.toString());
+
+const stre = fs.createWriteStream("nuevo3.yml");
+stre.once('open', function(fd) {
+    stre.write(doc.toString());
+  });
